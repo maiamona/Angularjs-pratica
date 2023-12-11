7cc5a4e3998162ea715a703a2f7b0933812d0ed2
Maiamona Garcia <maiamona.garcia@jupiter.co.ao>
2023-12-11 13:55:31 +0100

    4

--- js/app.js
+++ js/app.js
@@ -15,7 +15,4 @@ angular.module("meuMulo", ['ngRoute', 'ngMaterial'])
         
     });
      
-}).controller('AppCtrl', function($scope) {
-  $scope.isDisabled = true;
-  $scope.googleUrl = 'http://google.com';
-});
+})
